5c3b7d3cc2973571a1ae3b32de0006752c91b915
zelig81 <ilya.gorban@gmail.com>
2014-10-16 00:02:58 +0300

    clean up unused code

--- YetAnotherChess/src/project/ilyagorban/test/TestFigures.java
+++ YetAnotherChess/src/project/ilyagorban/test/TestFigures.java
@@ -39,38 +39,6 @@ public class TestFigures extends Main {
 	}
 	
 	@Test
-	public void testBishop() throws Exception {
-		this.setUp();
-		this.cvt.addMove("e2e4");
-		this.cvt.addMove("exit");
-		cc.start();
-		// b = Main.cm.getBoardObject();
-		// fig = b.getFigure("f1");
-		// assertEquals(
-		// "bishop at [" + fig.getXY()
-		// + "] should be able to move for 5 squares. its get "
-		// + b.getPossibleMoves(fig), b.getPossibleMoves(fig)
-		// .size(), 5);
-		// tearDown();
-		//
-		// setUp();
-		// cvt.addMove("e2e4");
-		// cvt.addMove("e7e5");
-		// cvt.addMove("f1e2");
-		// cvt.addMove("exit");
-		// cc.start();
-		// b = Main.cm.getBoardObject();
-		// fig = b.getFigure("e2");
-		// assertEquals(
-		// "bishop at [" + fig.getXY()
-		// + "] should be able to move for 8 squares. its get "
-		// + b.getPossibleMoves(fig), b.getPossibleMoves(fig)
-		// .size(), 8);
-		this.tearDown();
-		
-	}
-	
-	@Test
 	public void testKing() throws Exception {
 		this.setUp();
 		this.cvt.addMove("e2e4");
@@ -107,19 +75,6 @@ public class TestFigures extends Main {
 		this.cvt.addMove("b1c3");
 		this.cvt.addMove("h4f2");
 		this.cvt.addMove("exit");
-		// // ░|░A░| ░B░| ░C░| ░D░|░E░| ░F░| ░G░| ░H░|░
-		// // 8|▓♜▓|░░░|▓♝▓|░░░|▓♚▓|░♝░|▓♞▓|░♜░|8
-		// // 7|░♟░|▓▓▓|░♟░|▓♟▓|░░░|▓♟▓|░♟░|▓♟▓|7
-		// // 6|▓♟▓|░░░|▓♞▓|░░░|▓▓▓|░░░|▓▓▓|░░░|6
-		// // 5|░░░|▓▓▓|░░░|▓▓▓|░♟░|▓▓▓|░░░|▓▓▓|5
-		// // 4|▓▓▓|░░░|▓▓▓|░░░|▓♙▓|░░░|▓▓▓|░░░|4
-		// // 3|░░░|▓▓▓|░♘░|▓▓▓|░░░|▓♘▓|░░░|▓▓▓|3
-		// // 2|▓♙▓|░♙░|▓♙▓|░♙░|▓♕▓|░♛░|▓♙▓|░♙░|2
-		// // 1|░♖░|▓▓▓|░♗░|▓▓▓|░♔░|▓▓▓|░░░|▓♖▓|1
-		// // ░|░A░| ░B░| ░C░| ░D░|░E░| ░F░| ░G░| ░H░|░
-		// // WHITE your move (for example [e2e4]) or enter [exit] to quit the
-		// // game:
-		// // without check for check
 		// cc.start();
 		// b = Main.cm.getBoardObject();
 		// fig = b.getFigure("e8");
@@ -136,42 +91,6 @@ public class TestFigures extends Main {
 		// + fig.getXY()
 		// + "] should be able to move for 4 squares without check. its get "
 		// + pm, pm.size(), 4);
-		this.tearDown();
-	}
-	
-	@Test
-	public void testKnight() throws Exception {
-		this.setUp();
-		this.cvt.addMove("e2e4");
-		this.cvt.addMove("exit");
-		cc.start();
-		// b = Main.cm.getBoardObject();
-		// fig = b.getFigure("g1");
-		// assertEquals(
-		// fig.getRank() + " at [" + fig.getXY()
-		// + "] should be able to move for 3 squares. its get "
-		// + b.getPossibleMoves(fig), b.getPossibleMoves(fig)
-		// .size(), 3);
-		//
-		// fig = b.getFigure("b1");
-		// assertEquals(
-		// fig.getRank() + " at [" + fig.getXY()
-		// + "] should be able to move for 2 squares. its get "
-		// + b.getPossibleMoves(fig), b.getPossibleMoves(fig)
-		// .size(), 2);
-		this.tearDown();
-		
-		this.setUp();
-		this.cvt.addMove("e2e4");
-		this.cvt.addMove("b8c6");
-		this.cvt.addMove("exit");
-		cc.start();
-		// b = Main.cm.getBoardObject();
-		// fig = b.getFigure("c6");
-		// ArrayList<XY> pm = b.getPossibleMoves(fig);
-		// assertEquals(fig.getRank() + " at [" + fig.getXY()
-		// + "] should be able to move for 5 squares. its get " + pm,
-		// pm.size(), 5);
 		this.tearDown();
 	}
 	
@@ -232,73 +151,6 @@ public class TestFigures extends Main {
 		// assertEquals(fig.getRank() + " at [" + fig.getXY()
 		// + "] should be able to move for 3 squares. its get " + pm,
 		// pm.size(), 3);
-		//
-		this.tearDown();
-		
-	}
-	
-	@Test
-	public void testQueen() throws Exception {
-		this.setUp();
-		this.cvt.addMove("e2e4");
-		this.cvt.addMove("c7c5");
-		this.cvt.addMove("d1f3");
-		this.cvt.addMove("d8b6");
-		this.cvt.addMove("exit");
-		cc.start();
-		// b = Main.cm.getBoardObject();
-		// fig = b.getFigure("f3");
-		// pm = b.getPossibleMoves(fig);
-		// assertEquals(fig.getRank() + " at [" + fig.getXY()
-		// + "] should be able to move for 15 squares. its get " + pm,
-		// pm.size(), 15);
-		//
-		// fig = b.getFigure("b6");
-		// pm = b.getPossibleMoves(fig);
-		// assertEquals(fig.getRank() + " at [" + fig.getXY()
-		// + "] should be able to move for 14 squares. its get " + pm,
-		// pm.size(), 14);
-		this.tearDown();
-		
-	}
-	
-	@Test
-	public void testRook() throws Exception {
-		this.setUp();
-		this.cvt.addMove("a2a4");
-		this.cvt.addMove("a7a6");
-		this.cvt.addMove("h2h4");
-		this.cvt.addMove("h7h5");
-		this.cvt.addMove("a1a3");
-		this.cvt.addMove("a8a7");
-		this.cvt.addMove("h1h2");
-		this.cvt.addMove("h8h7");
-		this.cvt.addMove("exit");
-		cc.start();
-		// b = Main.cm.getBoardObject();
-		// fig = b.getFigure("a3");
-		// pm = b.getPossibleMoves(fig);
-		// assertEquals(fig.getRank() + " at [" + fig.getXY()
-		// + "] should be able to move for 2+7 squares. its get " + pm,
-		// pm.size(), 9);
-		//
-		// fig = b.getFigure("a7");
-		// pm = b.getPossibleMoves(fig);
-		// assertEquals(fig.getRank() + " at [" + fig.getXY()
-		// + "] should be able to move for 1 squares. its get " + pm,
-		// pm.size(), 1);
-		//
-		// fig = b.getFigure("h2");
-		// pm = b.getPossibleMoves(fig);
-		// assertEquals(fig.getRank() + " at [" + fig.getXY()
-		// + "] should be able to move for 2 squares. its get " + pm,
-		// pm.size(), 2);
-		//
-		// fig = b.getFigure("h7");
-		// pm = b.getPossibleMoves(fig);
-		// assertEquals(fig.getRank() + " at [" + fig.getXY()
-		// + "] should be able to move for 2 squares. its get " + pm,
-		// pm.size(), 2);
 		//
 		this.tearDown();
 		
