1b720b5ee5b38b22ed933e384012456611ef24fc
zelig81 <ilya.gorban@gmail.com>
2014-10-16 00:03:39 +0300

    add comment

--- YetAnotherChess/src/project/ilyagorban/model/figures/Pawn.java
+++ YetAnotherChess/src/project/ilyagorban/model/figures/Pawn.java
@@ -34,10 +34,10 @@ public class Pawn extends Figure {
 			if (isMovingOneSquare == true) {
 				output = CORRECT_MOVE;
 			}
-			
+			// check in XY.addToIndex should not make problem because isTouched checking
 			boolean isMovingTwoSquare =
 					output != CORRECT_MOVE && this.isTouched() == false && difXY[1] * dirY == 2
-					&& figTo == null && board[XY.addToIndex(this.getXY(), 0, dirY)] == null;
+							&& figTo == null && board[XY.addToIndex(this.getXY(), 0, dirY)] == null;
 			if (isMovingTwoSquare) {
 				output = CORRECT_MOVE;
 			}
