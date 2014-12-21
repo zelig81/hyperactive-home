60ac8208d6b140ea071c9690509c04fe379edc90
Ilya Gorban <ilya.gorban@gmail.com>
2014-12-21 23:17:45 +0200

    progress on ah72

--- AH72/src/ilyag/ah72/MainActivity.java
+++ AH72/src/ilyag/ah72/MainActivity.java
@@ -1,22 +1,23 @@
 package ilyag.ah72;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	EditText					et;
-	private final static int	dialog_id	= 234234234;
+	private final static int	dialog_id	= 234234;
 	private int					timer;
+	private Handler				handler		= new Handler();
+	private boolean				isPDShown;
 	Button						b;
 	
 	@Override
@@ -31,26 +32,85 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				String s = et.getText().toString();
 				if ("".equals(s)) {
-					timer = 0;
-				} else {
-					timer = Integer.parseInt(s);
+					Toast.makeText(getApplication(), "Please fill the timer field", Toast.LENGTH_LONG).show();
+					return;
 				}
+				timer = Integer.parseInt(s);
+				isPDShown = true;
 				showDialog(dialog_id);
+				Log.i("ilyag", "0");
+				/*
+				 * new Thread() {
+				 * 
+				 * @Override public void run() { while (true) { if (isPDShown == false) { break; } if (timer < 0) {
+				 * handler.post(new Runnable() {
+				 * 
+				 * @Override public void run() { MainActivity.this.finish(); } }); } try { Thread.sleep(100); } catch
+				 * (InterruptedException e) { Log.e("ilyag", e.getMessage()); } } } }.start();
+				 */
 			}
 		});
 	}
 	
 	@Override
 	protected Dialog onCreateDialog(int id) {
+		Log.i("ilyag", "1");
 		if (id == dialog_id) {
 			ProgressDialog pd = new ProgressDialog(getApplication());
 			pd.setTitle("The application will be closed after " + timer + ":");
 			pd.setCancelable(false);
-			
-			return builder.create();
+			pd.setIndeterminate(true);
+			pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			Log.i("ilyag", "2");
+			pd.setButton("Cancel closing", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+					isPDShown = false;
+				}
+			});
+			Log.i("ilyag", "3");
+			return pd;
 			
 		}
 		return super.onCreateDialog(id);
 		
+	}
+	
+	@Override
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		if (id == dialog_id) {
+			final ProgressDialog pd = (ProgressDialog) dialog;
+			pd.setMax(timer * 10);
+			pd.setProgress(timer * 10);
+			new Thread() {
+				@Override
+				public void run() {
+					for (int i = timer * 10; i > 0.1; i--) {
+						try {
+							Thread.sleep(100);
+						} catch (InterruptedException e) {
+							Log.e("ilyag", e.getMessage());
+						}
+						handler.post(new Runnable() {
+							
+							@Override
+							public void run() {
+								pd.incrementProgressBy(-1);
+							}
+						});
+					}
+					handler.post(new Runnable() {
+						
+						@Override
+						public void run() {
+							MainActivity.this.finish();
+						}
+					});
+				}
+			}.start();
+		}
+		super.onPrepareDialog(id, dialog);
 	}
 }
