caef338bf8429a6edaf50e7ec7665ad0ef0e20a8
Ilya Gorban <ilya.gorban@gmail.com>
2015-01-02 23:27:11 +0200

    in the middle ah82

--- AH82/app/src/main/java/ilyag/ah82/MainActivity.java
+++ AH82/app/src/main/java/ilyag/ah82/MainActivity.java
@@ -1,19 +1,73 @@
 package ilyag.ah82;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bRun = (Button)findViewById(R.id.bRun);
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_expandable_list_item_1);
+        ListView lv = (ListView)findViewById(R.id.listView);
+        lv.setAdapter(aa);
+        bRun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+                if (prev != null){
+                    ft.remove(prev);
+                }
+                ft.addToBackStack(null);
+                DialogFragment df = MyDialog.newInstance();
+                df.show(ft, "dialog");
+            }
+        });
     }
 
+    public static class MyDialog extends DialogFragment{
+        static MyDialog newInstance(){
+            return new MyDialog();
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Holo_Dialog);
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_dialog, container, false);
+            View bCancel = v.findViewById(R.id.bCancel);
+            ((Button)bCancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MyDialog.this.dismiss();
+                }
+            });
+            return v;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
