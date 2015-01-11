6bb3ed9ad9bb8c108d23ee97a8cd62826ff507ef
Ilya Gorban <ilya.gorban@gmail.com>
2015-01-11 06:29:11 +0200

    ah92 mid

--- AH92/app/src/main/java/ilyag/ah92/MakePhotoActivity.java
+++ AH92/app/src/main/java/ilyag/ah92/MakePhotoActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MakePhotoActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_make_photo);
     }
 
 
--- AH92/app/src/main/java/ilyag/ah92/ShowPhotosActivity.java
+++ AH92/app/src/main/java/ilyag/ah92/ShowPhotosActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ShowPhotosActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_show_photos);
     }
 
 
