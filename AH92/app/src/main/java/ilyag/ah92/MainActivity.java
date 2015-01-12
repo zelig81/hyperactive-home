0cb72e81e62360e68c8462181051c1a0ab530b2e
Ilya Gorban <ilya.gorban@gmail.com>
2015-01-12 20:05:31 +0200

    ah92 mid

--- AH92/app/src/main/java/ilyag/ah92/MainActivity.java
+++ AH92/app/src/main/java/ilyag/ah92/MainActivity.java
@@ -2,14 +2,16 @@ package ilyag.ah92;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    Button bMake, bShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
