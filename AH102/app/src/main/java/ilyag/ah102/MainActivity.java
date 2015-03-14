7e311e32cf8ee55440a398324823d804f70c9de0
Ilya Gorban <ilya.gorban@gmail.com>
2015-03-14 20:10:20 +0200

    ah102 before channels use

--- AH102/app/src/main/java/ilyag/ah102/MainActivity.java
+++ AH102/app/src/main/java/ilyag/ah102/MainActivity.java
@@ -4,26 +4,50 @@ import android.app.Activity;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
-    TextView tvUser, tvMessage;
+    static TextView tvUser;
+    static TextView tvMessage;
     EditText et;
     Button bSend;
-    Spinner spinner;
-    ToggleButton tbSignInOut;
+    static Spinner spinner;
+    static ToggleButton tbSignInOut;
+    static ParseUser puCurrentUser;
+    static ParseInstallation piCurrentInstallation;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            login(ParseUser.getCurrentUser(), this);
+        } catch (ParseException e) {
+            Log.e("ilyag1", e.getMessage());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +60,8 @@ public class MainActivity extends Activity {
         et = (EditText) findViewById(R.id.editText);
         bSend = (Button) findViewById(R.id.bSend);
         tbSignInOut = (ToggleButton) findViewById(R.id.tbSignIn);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        piCurrentInstallation = ParseInstallation.getCurrentInstallation();
 
         bSend.setOnClickListener(new View.OnClickListener() {
 
@@ -43,8 +69,8 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 ParseUser user = ParseUser.getCurrentUser();
                 if (user == null) {
-                    tvMessage.setText("no user");
-                }else{
+                    tvMessage.setText("no signed user");
+                } else {
                     tvMessage.setText(user.getObjectId() + " / " + user.getUsername());
                 }
             }
@@ -53,20 +79,18 @@ public class MainActivity extends Activity {
         tbSignInOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!tbSignInOut.isChecked()){
-                    ParseUser.logOut();
-                    tvUser.setText("no user");
-                }else{
+                if (!tbSignInOut.isChecked()) {
+                    logout();
+                } else {
                     FragmentTransaction ft = getFragmentManager().beginTransaction();
                     Fragment fragment = getFragmentManager().findFragmentByTag("ah102dialog");
-
-                    try {
-                        ParseUser.logIn("user1", "123");
-                        tvUser.setText("user1");
-                    } catch (ParseException e) {
-                        Log.e("ilyag1", "no such user");
-                        tvMessage.setText("no such user");
+                    if (fragment != null) {
+                        ft.remove(fragment);
                     }
+                    ft.addToBackStack(null);
+
+                    DialogFragment df = MyDialogFragment.newInstance();
+                    df.show(ft, "ah102dialog");
 
                 }
             }
@@ -76,6 +100,101 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
+        if (puCurrentUser != null) {
+            ParseInstallation.getCurrentInstallation();
+            try {
+                puCurrentUser.save();
+            } catch (ParseException e) {
+                Log.e("ilyag", e.getMessage());
+            }
+        }
 
+    }
+
+    public static class MyDialogFragment extends DialogFragment {
+        EditText etUser, etPassword;
+        Button bCancel, bOK;
+
+        public static DialogFragment newInstance() {
+            return new MyDialogFragment();
+        }
+
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.dialog_fragment, container, false);
+            etUser = (EditText) v.findViewById(R.id.etUser);
+            etPassword = (EditText) v.findViewById(R.id.etPassword);
+            bCancel = (Button) v.findViewById(R.id.bCancel);
+            bOK = (Button) v.findViewById(R.id.bLogIn);
+
+            bCancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dismiss();
+                }
+            });
+
+            bOK.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        String sUser = etUser.getText().toString();
+                        String sPassword = etPassword.getText().toString();
+                        if ("".equals(sUser)) {
+                            Toast.makeText(getActivity(), "Don't forget to type username", Toast.LENGTH_LONG).show();
+                        } else {
+                            login(sUser, sPassword, getActivity());
+                            dismiss();
+                        }
+                    } catch (ParseException e) {
+                        Log.e("ilyag1", "no such etUser");
+                        Toast.makeText(getActivity(), "no such user", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+            return v;
+        }
+
+    }
+    public static boolean login(String user, String password, Context context) throws ParseException {
+        ParseUser loggedUser = ParseUser.logIn(user, password);
+        if (loggedUser == null){
+            return false;
+        }else {
+            return login(loggedUser, context);
+        }
+    }
+    public static boolean login(ParseUser puUser, Context context) throws ParseException {
+        if (puUser != null) {
+            tvUser.setText(puUser.getUsername());
+            puCurrentUser = ParseUser.getCurrentUser();
+            ParseQuery<ParseUser> query = ParseUser.getQuery();
+            query.whereNotEqualTo("username", puUser.getUsername());
+            List<ParseUser> puList = query.find();
+            List<String> userList = new ArrayList<String>();
+            for (ParseUser pu : puList) {
+                userList.add(pu.getUsername());
+            }
+            ArrayAdapter listAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, userList);
+            spinner.setAdapter(listAdapter);
+
+            tbSignInOut.setChecked(true);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void logout() {
+        ParseUser.logOut();
+        spinner.setAdapter(null);
+        List<String> channels = piCurrentInstallation.getList("channels");
+        if ()
+        puCurrentUser = null;
+        tbSignInOut.setChecked(false);
+        tvUser.setText("no user");
     }
 }
