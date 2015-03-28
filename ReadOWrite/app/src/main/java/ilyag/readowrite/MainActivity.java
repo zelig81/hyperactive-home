a09e94305396189bbd5a740d248f32805f5d7650
Ilya Gorban <ilya.gorban@gmail.com>
2015-03-28 19:31:52 +0300

    readowrite swipe works

--- ReadOWrite/app/src/main/java/ilyag/readowrite/MainActivity.java
+++ ReadOWrite/app/src/main/java/ilyag/readowrite/MainActivity.java
@@ -1,20 +1,64 @@
 package ilyag.readowrite;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements WriteFragment.OnFragmentInteractionListener {
+    Fragment readFragment, writeFragment;
+    boolean isRead = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readFragment = new ReadFragment();
+        writeFragment = new WriteFragment();
 
+        View fragmentContainer = findViewById(R.id.fragment_container);
+
+        if (fragmentContainer != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            getFragmentManager().beginTransaction().add(R.id.fragment_container, readFragment).commit();
+        }
+
+        fragmentContainer.setOnTouchListener(new OnSwipeTouchListener(this) {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+                move();
+            }
+
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+                move();
+            }
+        });
     }
 
+
+    private void move() {
+        if (isRead) {
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container, writeFragment).commit();
+            isRead = false;
+        } else {
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container, readFragment).commit();
+            isRead = true;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,4 +82,8 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
