fb24c8e72766daf70114f9b3b9033f31984e3234
Ilya <ilya.gorban@gmail.com>
2015-04-06 15:38:48 +0300

    row reader view alpha

--- ReadOWrite/app/src/main/java/ilyag/readowrite/MainActivity.java
+++ ReadOWrite/app/src/main/java/ilyag/readowrite/MainActivity.java
@@ -1,97 +1,132 @@
 package ilyag.readowrite;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.net.Uri;
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+
+import java.lang.reflect.Field;
+import java.util.Locale;
 
 
-public class MainActivity extends Activity implements WriteFragment.OnFragmentInteractionListener, ReadFragment.OnFragmentInteractionListener {
-    Fragment readFragment, writeFragment;
+public class MainActivity extends FragmentActivity {
     boolean isRead = true;
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        readFragment = new ReadFragment();
-        writeFragment = new WriteFragment();
-
-
-
-        View fragmentContainer = findViewById(R.id.fragment_container);
-
-        if (fragmentContainer != null) {
-            if (savedInstanceState != null) {
-                return;
+        
+        ActionBar actionBar = getActionBar();
+        actionBar.show();
+        try {
+            ViewConfiguration vc = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(vc, false);
             }
-            getFragmentManager().beginTransaction().add(R.id.fragment_container, readFragment).commit();
+        } catch (NoSuchFieldException e) {
+            Log.e("ilyag1", e.getMessage());
+        } catch (IllegalAccessException e) {
+            Log.e("ilyag1", e.getMessage());
         }
 
-        fragmentContainer.setOnTouchListener(new OnSwipeTouchListener(this) {
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return gestureDetector !=null && gestureDetector.onTouchEvent(event);
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-            }
-
-            @Override
-            public void onSwipeRight() {
-                if (isRead) {
-                    move();
-                }
-            }
-
-            @Override
-            public void onSwipeLeft() {
-                if (!isRead) {
-                    move();
-                }
-            }
-        });
     }
 
-
-    private void move() {
-        if (isRead) {
-            getFragmentManager().beginTransaction().replace(R.id.fragment_container, writeFragment).commit();
-            isRead = false;
-        } else {
-            getFragmentManager().beginTransaction().replace(R.id.fragment_container, readFragment).commit();
-            isRead = true;
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_search) {
+            return true;
+        } else if (id == R.id.action_refresh) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
 
+    /**
+     * A {@link android.support.v4.app.FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new ReadUpdatesFragment();
+                case 1:
+                    return new ReadMyshelfFragment();
+                case 2:
+                    return new ReadSearchFragment();
+                default:
+                    return null;
+            }
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            //return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3).toUpperCase(l);
+            }
+            return null;
+        }
     }
+
+
 }
